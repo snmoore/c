b8e3d71f2d5e3ba4fab7fe6fe02ec13080cc173b
Simon Moore <simon@snmoore.net>
2016-04-01 18:45:28 +0100

    Fix the 64-bit limit checks and adjust the other tests for consistency

--- ctoolkit/snm_types/test/src/snm_test_types_limits.c
+++ ctoolkit/snm_types/test/src/snm_test_types_limits.c
@@ -36,6 +36,8 @@
  * -----------------------------------------------------------------------------
  * 20050130 1       simon@snmoore.net   Add initial types support
  * 20050202 2       simon@snmoore.net   Add initial debug support
+ * 20160401 3       simon@snmoore.net   Fix the 64-bit limit checks and adjust
+ *                                      the other tests for consistency
  */
 
 
@@ -54,43 +56,28 @@
 
 /*
  * The limits here are generally tested by setting explicit bits and using
- * using bitwise operations, rather than relying upon (size dependent)
- * hand-coded values, arithmetic operations and overflow behaviour; e.g.:
+ * using bitwise operations, rather than relying upon hand-coded values (that
+ * are size dependent), arithmetic operations or overflow behaviour.
  *
- *      signed min:     sign bit on, all others off
- *                      (1 << ((sizeof(type) * CHAR_BIT) - 1))
+ * For signed min, treat as unsigned and set the top bit, then cast, e.g.:
+ *      type MinValue = (type)1u << ((sizeof(type) * CHAR_BIT) - 1);
  *
- *      signed max:     sign bit off, all others on
- *                      ~(1 << ((sizeof(type) * CHAR_BIT) - 1))
- *
- *      unsigned max:   all bits on
- *                      ~0
- *
- * But for signed integers, manipulating the sign bit directly results in
- * overflow warnings (at least with GCC), even though the resulting values are
- * correct; hence more tedious solutions are required
- *
- * For signed max, use a loop to shift each relevant bit into place:
+ * For signed max, use a loop to shift each relevant bit into place, e.g.:
+ *      size_t i;
  *      type MaxValue = 0;
- *      for(i = (sizeof(type) * CHAR_BIT) - 2; i >= 0; i--) {
+ *      for(i = 0; i < ((sizeof(type) * CHAR_BIT) - 1); i++) {
  *          MaxValue |= 1 << i;
  *      }
  *
- * For signed min, just generate the complement of the signed max value 
+ * For unsigned max, take the complement of zero to set all bits, e.g.:
+ *      type MaxValue = ~0;
  */
 
 /* int8_t minimum is INT8_MIN */
 SNM_TEST_RESULT snm_test_types_limits_1(void) {
-    intmax_t i;
-    int8_t MaxValue = 0;
-    int8_t MinValue = 0;
+    int8_t MinValue = (int8_t)1u << ((sizeof(int8_t) * CHAR_BIT) - 1);
 
-    for(i = (sizeof(int8_t) * CHAR_BIT) - 2; i >= 0; i--) {
-        MaxValue |= 1 << i;
-    }
-    MinValue = ~MaxValue;
-
-    snm_debug_trivial("MinValue: %hi, 0x%hx, INT8_MIN: %hi\n",
+    snm_debug_trivial("MinValue: %hhi, 0x%hhx, INT8_MIN: %hhi",
                       MinValue, MinValue & 0xFF, INT8_MIN);
 
     return (INT8_MIN == MinValue) ? SNM_TEST_PASS : SNM_TEST_FAIL;
@@ -98,14 +85,13 @@ SNM_TEST_RESULT snm_test_types_limits_1(void) {
 
 /* int8_t maximum is INT8_MAX */
 SNM_TEST_RESULT snm_test_types_limits_2(void) {
-    intmax_t i;
+    size_t i;
     int8_t MaxValue = 0;
-
-    for(i = (sizeof(int8_t) * CHAR_BIT) - 2; i >= 0; i--) {
+    for(i = 0; i < ((sizeof(int8_t) * CHAR_BIT) - 1); i++) {
         MaxValue |= 1 << i;
     }
 
-    snm_debug_trivial("MaxValue: %hi, 0x%hx, INT8_MAX: %hi\n",
+    snm_debug_trivial("MaxValue: %hhi, 0x%hhx, INT8_MAX: %hhi",
                       MaxValue, MaxValue & 0xFF, INT8_MAX);
 
     return (INT8_MAX == MaxValue) ? SNM_TEST_PASS : SNM_TEST_FAIL;
@@ -115,7 +101,7 @@ SNM_TEST_RESULT snm_test_types_limits_2(void) {
 SNM_TEST_RESULT snm_test_types_limits_3(void) {
     uint8_t MaxValue = ~0;
 
-    snm_debug_trivial("MaxValue: %hu, 0x%hx, UINT8_MAX: %hu\n",
+    snm_debug_trivial("MaxValue: %hhu, 0x%hhx, UINT8_MAX: %hhu",
                       MaxValue, MaxValue & 0xFF, UINT8_MAX);
 
     return (UINT8_MAX == MaxValue) ? SNM_TEST_PASS : SNM_TEST_FAIL;
@@ -123,16 +109,9 @@ SNM_TEST_RESULT snm_test_types_limits_3(void) {
 
 /* int16_t minimum is INT16_MIN */
 SNM_TEST_RESULT snm_test_types_limits_4(void) {
-    intmax_t i;
-    int16_t MaxValue = 0;
-    int16_t MinValue = 0;
+    int16_t MinValue = (int16_t)1u << ((sizeof(int16_t) * CHAR_BIT) - 1);
 
-    for(i = (sizeof(int16_t) * CHAR_BIT) - 2; i >= 0; i--) {
-        MaxValue |= 1 << i;
-    }
-    MinValue = ~MaxValue;
-
-    snm_debug_trivial("MinValue: %hi, 0x%hx, INT16_MIN: %hi\n",
+    snm_debug_trivial("MinValue: %hi, 0x%hx, INT16_MIN: %hi",
                       MinValue, MinValue, INT16_MIN);
 
     return (INT16_MIN == MinValue) ? SNM_TEST_PASS : SNM_TEST_FAIL;
@@ -140,14 +119,13 @@ SNM_TEST_RESULT snm_test_types_limits_4(void) {
 
 /* int16_t maximum is INT16_MAX */
 SNM_TEST_RESULT snm_test_types_limits_5(void) {
-    intmax_t i;
+    size_t i;
     int16_t MaxValue = 0;
-
-    for(i = (sizeof(int16_t) * CHAR_BIT) - 2; i >= 0; i--) {
+    for(i = 0; i < ((sizeof(int16_t) * CHAR_BIT) - 1); i++) {
         MaxValue |= 1 << i;
     }
 
-    snm_debug_trivial("MaxValue: %hi, 0x%hx, INT16_MAX: %hi\n",
+    snm_debug_trivial("MaxValue: %hi, 0x%hx, INT16_MAX: %hi",
                       MaxValue, MaxValue, INT16_MAX);
 
     return (INT16_MAX == MaxValue) ? SNM_TEST_PASS : SNM_TEST_FAIL;
@@ -157,7 +135,7 @@ SNM_TEST_RESULT snm_test_types_limits_5(void) {
 SNM_TEST_RESULT snm_test_types_limits_6(void) {
     uint16_t MaxValue = ~0;
 
-    snm_debug_trivial("MaxValue: %hu, 0x%hx, UINT16_MAX: %hu\n",
+    snm_debug_trivial("MaxValue: %hu, 0x%hx, UINT16_MAX: %hu",
         MaxValue, MaxValue, UINT16_MAX);
 
     return (UINT16_MAX == MaxValue) ? SNM_TEST_PASS : SNM_TEST_FAIL;
@@ -165,16 +143,9 @@ SNM_TEST_RESULT snm_test_types_limits_6(void) {
 
 /* int32_t minimum is INT32_MIN */
 SNM_TEST_RESULT snm_test_types_limits_7(void) {
-    intmax_t i;
-    int32_t MaxValue = 0;
-    int32_t MinValue = 0;
+    int32_t MinValue = (int32_t)1u << ((sizeof(int32_t) * CHAR_BIT) - 1);
 
-    for(i = (sizeof(int32_t) * CHAR_BIT) - 2; i >= 0; i--) {
-        MaxValue |= 1 << i;
-    }
-    MinValue = ~MaxValue;
-
-    snm_debug_trivial("MinValue: %i, 0x%x, INT32_MIN: %i\n",
+    snm_debug_trivial("MinValue: %i, 0x%x, INT32_MIN: %i",
                       MinValue, MinValue, INT32_MIN);
 
     return (INT32_MIN == MinValue) ? SNM_TEST_PASS : SNM_TEST_FAIL;
@@ -182,14 +153,13 @@ SNM_TEST_RESULT snm_test_types_limits_7(void) {
 
 /* int32_t maximum is INT32_MAX */
 SNM_TEST_RESULT snm_test_types_limits_8(void) {
-    intmax_t i;
+    size_t i;
     int32_t MaxValue = 0;
-
-    for(i = (sizeof(int32_t) * CHAR_BIT) - 2; i >= 0; i--) {
+    for(i = 0; i < ((sizeof(int32_t) * CHAR_BIT) - 1); i++) {
         MaxValue |= 1 << i;
     }
 
-    snm_debug_trivial("MaxValue: %i, 0x%x, INT32_MAX: %i\n",
+    snm_debug_trivial("MaxValue: %i, 0x%x, INT32_MAX: %i",
                       MaxValue, MaxValue, INT32_MAX);
 
     return (INT32_MAX == MaxValue) ? SNM_TEST_PASS : SNM_TEST_FAIL;
@@ -199,7 +169,7 @@ SNM_TEST_RESULT snm_test_types_limits_8(void) {
 SNM_TEST_RESULT snm_test_types_limits_9(void) {
     uint32_t MaxValue = ~0;
 
-    snm_debug_trivial("MaxValue: %u, 0x%x, UINT32_MAX: %u\n",
+    snm_debug_trivial("MaxValue: %u, 0x%x, UINT32_MAX: %u",
                       MaxValue, MaxValue, UINT32_MAX);
 
     return (UINT32_MAX == MaxValue) ? SNM_TEST_PASS : SNM_TEST_FAIL;
@@ -208,16 +178,9 @@ SNM_TEST_RESULT snm_test_types_limits_9(void) {
 /* int64_t minimum is INT64_MIN */
 SNM_TEST_RESULT snm_test_types_limits_10(void) {
     #if defined(SNM_64BIT_TYPES_AVAILABLE)
-        intmax_t i;
-        int64_t MaxValue = 0;
-        int64_t MinValue = 0;
+        int64_t MinValue = (int64_t)1llu << ((sizeof(int64_t) * CHAR_BIT) - 1);
 
-        for(i = (sizeof(int64_t) * CHAR_BIT) - 2; i >= 0; i--) {
-            MaxValue |= 1 << i;
-        }
-        MinValue = ~MaxValue; 
-
-        snm_debug_trivial("MinValue: %lli, 0x%llx, INT64_MIN: %lli\n",
+        snm_debug_trivial("MinValue: %lli, 0x%llx, INT64_MIN: %lli",
                           MinValue, MinValue, INT64_MIN);
  
         return (INT64_MIN == MinValue) ? SNM_TEST_PASS : SNM_TEST_FAIL;
@@ -229,14 +192,13 @@ SNM_TEST_RESULT snm_test_types_limits_10(void) {
 /* int64_t maximum is INT64_MAX */
 SNM_TEST_RESULT snm_test_types_limits_11(void) {
     #if defined(SNM_64BIT_TYPES_AVAILABLE)
-        intmax_t i;
+        size_t i;
         int64_t MaxValue = 0;
-
-        for(i = (sizeof(int64_t) * CHAR_BIT) - 2; i >= 0; i--) {
-            MaxValue |= 1 << i;
+        for(i = 0; i < ((sizeof(int64_t) * CHAR_BIT) - 1); i++) {
+            MaxValue |= 1ll << i;
         }
 
-        snm_debug_trivial("MaxValue: %lli, 0x%llx, INT64_MAX: %lli\n",
+        snm_debug_trivial("MaxValue: %lli, 0x%llx, INT64_MAX: %lli",
                           MaxValue, MaxValue, INT64_MAX);
 
         return (INT64_MAX == MaxValue) ? SNM_TEST_PASS : SNM_TEST_FAIL;
@@ -250,7 +212,7 @@ SNM_TEST_RESULT snm_test_types_limits_12(void) {
     #if defined(SNM_64BIT_TYPES_AVAILABLE)
         uint64_t MaxValue = ~0;
 
-        snm_debug_trivial("MaxValue: %llu, 0x%llx, UINT64_MAX: %llu\n",
+        snm_debug_trivial("MaxValue: %llu, 0x%llx, UINT64_MAX: %llu",
                           MaxValue, MaxValue, UINT64_MAX);
 
         return (UINT64_MAX == MaxValue) ? SNM_TEST_PASS : SNM_TEST_FAIL;
@@ -261,20 +223,15 @@ SNM_TEST_RESULT snm_test_types_limits_12(void) {
 
 /* intmax_t minimum is INTMAX_MIN */
 SNM_TEST_RESULT snm_test_types_limits_13(void) {
-    intmax_t i;
-    intmax_t MaxValue = 0;
-    intmax_t MinValue = 0;
-
-    for(i = (sizeof(intmax_t) * CHAR_BIT) - 2; i >= 0; i--) {
-        MaxValue |= 1 << i;
-    }
-    MinValue = ~MaxValue;
-
     #if defined(SNM_64BIT_TYPES_AVAILABLE)
-        snm_debug_trivial("MinValue: %lli, 0x%llx, INTMAX_MIN: %lli\n",
+        intmax_t MinValue = (intmax_t)1llu << ((sizeof(intmax_t) * CHAR_BIT) - 1);
+
+        snm_debug_trivial("MinValue: %lli, 0x%llx, INTMAX_MIN: %lli",
                           MinValue, MinValue, INTMAX_MIN);
     #else
-        snm_debug_trivial("MinValue: %li, 0x%lx, INTMAX_MIN: %li\n",
+        intmax_t MinValue = (intmax_t)1u << ((sizeof(intmax_t) * CHAR_BIT) - 1);
+
+        snm_debug_trivial("MinValue: %i, 0x%x, INTMAX_MIN: %i",
                           MinValue, MinValue, INTMAX_MIN);    
     #endif
 
@@ -283,18 +240,23 @@ SNM_TEST_RESULT snm_test_types_limits_13(void) {
 
 /* intmax_t maximum is INTMAX_MAX */
 SNM_TEST_RESULT snm_test_types_limits_14(void) {
-    intmax_t i;
-    intmax_t MaxValue = 0;
-
-    for(i = (sizeof(intmax_t) * CHAR_BIT) - 2; i >= 0; i--) {
-        MaxValue |= 1 << i;
-    }
-
     #if defined(SNM_64BIT_TYPES_AVAILABLE)
-        snm_debug_trivial("MaxValue: %lli, 0x%llx, INTMAX_MAX: %lli\n",
+        size_t i;
+        intmax_t MaxValue = 0;
+        for(i = 0; i < ((sizeof(intmax_t) * CHAR_BIT) - 1); i++) {
+            MaxValue |= 1ll << i;
+        }
+
+        snm_debug_trivial("MaxValue: %lli, 0x%llx, INTMAX_MAX: %lli",
                           MaxValue, MaxValue, INTMAX_MAX);
     #else
-        snm_debug_trivial("MaxValue: %li, 0x%lx, INTMAX_MAX: %li\n",
+        size_t i;
+        intmax_t MaxValue = 0;
+        for(i = 0; i < ((sizeof(intmax_t) * CHAR_BIT) - 1); i++) {
+            MaxValue |= 1 << i;
+        }
+
+        snm_debug_trivial("MaxValue: %i, 0x%x, INTMAX_MAX: %i",
                           MaxValue, MaxValue, INTMAX_MAX);    
     #endif
 
@@ -306,10 +268,10 @@ SNM_TEST_RESULT snm_test_types_limits_15(void) {
     uintmax_t MaxValue = ~0;
 
     #if defined(SNM_64BIT_TYPES_AVAILABLE)
-        snm_debug_trivial("MaxValue: %llu, 0x%llx, UINTMAX_MAX: %llu\n",
+        snm_debug_trivial("MaxValue: %llu, 0x%llx, UINTMAX_MAX: %llu",
                           MaxValue, MaxValue, UINTMAX_MAX);
     #else
-        snm_debug_trivial("MaxValue: %lu, 0x%lx, UINTMAX_MAX: %lu\n",
+        snm_debug_trivial("MaxValue: %u, 0x%x, UINTMAX_MAX: %u",
                           MaxValue, MaxValue, UINTMAX_MAX);    
     #endif
 
@@ -345,16 +307,9 @@ SNM_TEST_RESULT snm_test_types_limits_18(void) {
 
 /* char_t minimum is SCHAR_MIN */
 SNM_TEST_RESULT snm_test_types_limits_19(void) {
-    intmax_t i;
-    char_t MaxValue = 0;
-    char_t MinValue = 0;
+    char_t MinValue = (char_t)1u << ((sizeof(char_t) * CHAR_BIT) - 1);
 
-    for(i = (sizeof(char_t) * CHAR_BIT) - 2; i >= 0; i--) {
-        MaxValue |= 1 << i;
-    }
-    MinValue = ~MaxValue;
-
-    snm_debug_trivial("MinValue: %hi, 0x%hx, SCHAR_MIN: %hi\n",
+    snm_debug_trivial("MinValue: %hhi, 0x%hhx, SCHAR_MIN: %hhi",
                       MinValue, MinValue & 0xFF, SCHAR_MIN);
 
     return (SCHAR_MIN == MinValue) ? SNM_TEST_PASS : SNM_TEST_FAIL;
@@ -362,14 +317,13 @@ SNM_TEST_RESULT snm_test_types_limits_19(void) {
 
 /* char_t maximum is SCHAR_MAX */
 SNM_TEST_RESULT snm_test_types_limits_20(void) {
-    intmax_t i;
+    size_t i;
     char_t MaxValue = 0;
-
-    for(i = (sizeof(char_t) * CHAR_BIT) - 2; i >= 0; i--) {
+    for(i = 0; i < ((sizeof(char_t) * CHAR_BIT) - 1); i++) {
         MaxValue |= 1 << i;
     }
 
-    snm_debug_trivial("MaxValue: %hi, 0x%hx, SCHAR_MAX: %hi\n",
+    snm_debug_trivial("MaxValue: %hhi, 0x%hhx, SCHAR_MAX: %hhi",
                       MaxValue, MaxValue & 0xFF, SCHAR_MAX);
 
     return (SCHAR_MAX == MaxValue) ? SNM_TEST_PASS : SNM_TEST_FAIL;
@@ -379,7 +333,7 @@ SNM_TEST_RESULT snm_test_types_limits_20(void) {
 SNM_TEST_RESULT snm_test_types_limits_21(void) {
     uchar_t MaxValue = ~0;
 
-    snm_debug_trivial("MaxValue: %hu, 0x%hx, UCHAR_MAX: %hu\n",
+    snm_debug_trivial("MaxValue: %hhu, 0x%hhx, UCHAR_MAX: %hhu",
                       MaxValue, MaxValue & 0xFF, UCHAR_MAX);
 
     return (UCHAR_MAX == MaxValue) ? SNM_TEST_PASS : SNM_TEST_FAIL;
@@ -389,7 +343,7 @@ SNM_TEST_RESULT snm_test_types_limits_21(void) {
 SNM_TEST_RESULT snm_test_types_limits_22(void) {
     utf8_t MaxValue = ~0;
 
-    snm_debug_trivial("MaxValue: %hu, 0x%hx, UINT8_MAX: %hu\n",
+    snm_debug_trivial("MaxValue: %hhu, 0x%hhx, UINT8_MAX: %hhu",
                       MaxValue, MaxValue & 0xFF, UINT8_MAX);
 
     return (UINT8_MAX == MaxValue) ? SNM_TEST_PASS : SNM_TEST_FAIL;
@@ -399,7 +353,7 @@ SNM_TEST_RESULT snm_test_types_limits_22(void) {
 SNM_TEST_RESULT snm_test_types_limits_23(void) {
     utf16_t MaxValue = ~0;
 
-    snm_debug_trivial("MaxValue: %hu, 0x%hx, UINT16_MAX: %hu\n",
+    snm_debug_trivial("MaxValue: %hu, 0x%hx, UINT16_MAX: %hu",
                       MaxValue, MaxValue, UINT16_MAX);
 
     return (UINT16_MAX == MaxValue) ? SNM_TEST_PASS : SNM_TEST_FAIL;
